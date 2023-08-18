0093bcd4d6c8e54bbd7a703de22d87c2a8cc866f
cwaiHangRen <137182468+cwaiHangRen@users.noreply.github.com>
2023-08-18 22:51:33 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wwx035aac8de81413a5",
+  appsecret: "08c626a17997a072a8da4550867d77e3",
+  template_id: "	CEVHhxShqG6j5sfgfDIDMZ7XXZ8HrI5lCYjSjpybh8I",
+  touser: "oPRks670JSehpXiBtCMIM-7bD0cE",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "北京",
+  first_date: "2022-3-10",
 };
